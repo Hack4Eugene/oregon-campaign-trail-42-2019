7bfff0df56745aa1718c234f3074d48559f4fb91
Daniel <coatsd@my.lanecc.edu>
2019-04-27 21:30:32 -0700

    Fixed the old Validator file to be syntactically correct.

--- src/js/SceneValidator.js
+++ src/js/SceneValidator.js
@@ -1,20 +1,18 @@
 import Scenes from scenes.js;
 
 //this.scenes = Scenes
-
-ValidateImageEndpoints() {
-	Scenes.Foreach(sOut => {
-		sOut["choices"].Foreach(c => {
+validateEventEndpoints() {
+	this.scenes.forEach((sOut) => {
+		sOut["choices"].forEach((c) => {
 			let hasMatch = false;
 			let sceneName = c["sceneDestination"];
-			Scenes.Foreach(sIn => {
+			Scenes.forEach((sIn) => {
 				if (sIn["name"] == sceneName) {
 					hasMatch = true;
-					break;
 				}
 			});
 			if (!hasMatch) {
-				console.log("Scene: " sOut["name"] + " Decision: " + c["description"] + " has no match!");
+				console.log("Scene: " + sOut["name"] + " Decision: " + c["description"] + " has no match!");
 			}
 		});
 	});
