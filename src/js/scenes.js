d628f0042015657d38a22e7cef81a5b08d2e173c
jakeism <jakeism@gmail.com>
2019-04-27 22:09:12 -0700

    Scene 16 Up To

--- src/js/scenes.js
+++ src/js/scenes.js
@@ -5,6 +5,7 @@ let Scenes =  [
 		"question" : "At a school board meeting in April 2020, the school board surprises the community with plans to change the location of your local school. You are upset by these plans, and take the opportunity to speak out against the move. ", // Question 
 		"current_date" : "April 2020", // Date for Month
 		"campaign_month_count" : 0, // Counter for Budget
+		"img" : "Animation_School.gif",
 		"choices" : [
 				{
 					"description" : "Continue",
@@ -22,7 +23,7 @@ let Scenes =  [
 
 		"current_date" : "April 2020", // Date for Month
 		"campaign_month_count" : 0, // Counter for Budget
-		"img" : "Animation_School.gif",
+		"img" : "Animation_Heather_Walking.gif",
 		"choices" : [
 				{
 					"description" : "Continue",
@@ -40,7 +41,7 @@ let Scenes =  [
 
 		"current_date" : "April 2020", // Date for Month
 		"campaign_month_count" : 0, // Counter for Budget
-		"img": "Animation_Heather_Walking.gif",
+		"img": "Animation_Heather_Coffee.gif",
 		"choices" : [
 				{
 					"description" : "Continue",
@@ -55,7 +56,7 @@ let Scenes =  [
 		`,
 		"current_date" : "May 2020", // Date for Month
 		"campaign_month_count" : 1, // Counter for Budget
-		"img" : "heather_coffee.gif",
+		"img" : "Animation_Heather_Planning.gif",
 		"choices" : [
 				{
 					"description" : "Rent an office to run your campaign from",
@@ -84,6 +85,7 @@ let Scenes =  [
 		"question" : `Heather responds with “I’d find someone to manage my campaign and design a logo first, but this could work too!”`,
 		"current_date" : "May 2020", // Date for Month
 		"campaign_month_count" : 1, // Counter for Budget
+		"img" : "Animation_Heather_Planning.gif",
 		"choices" : [
 				{
 					"description" : "Continue",
@@ -97,6 +99,7 @@ let Scenes =  [
 		"question" : `Heather responds with, “A campaign manager and someone to design a logo would be my top priority too!”`,
 		"current_date" : "May 2020", // Date for Month
 		"campaign_month_count" : 1, // Counter for Budget
+		"img" : "Animation_Heather_Planning.gif",
 		"choices" : [
 				{
 					"description" : "Continue",
@@ -110,6 +113,7 @@ let Scenes =  [
 		"question" : `It’s time to set your budget! Click on the “budget” button to set up your campaign budget. Each item will cost money. Items may generate additional income, and even give bonuses to your polling percentage.`,
 		"current_date" : "May 2020", // Date for Month
 		"campaign_month_count" : 1, // Counter for Budget
+		"img" : "Animation_Heather_Planning.gif",
 		"choices" : [
 				{
 					"description" : "Continue",
@@ -409,17 +413,160 @@ let Scenes =  [
 	},
 	{ 
 		"name": "screen_14",
-		"question" : ``,
-		"current_date" : "May 2020", // Date for Month
-		"campaign_month_count" : 1, // Counter for Budget
+		"question" : `You’re reading the paper one morning in October, and notice that there’s a letter to the editor supporting one of your opponents in the race. You realize that you need to get your name in the paper somehow too. What’s the best way to achieve this goal?`,
+		"current_date" : "October 2020", // Date for Month
+		"campaign_month_count" : 6, // Counter for Budget
+		"choices" : [
+			{
+				"description" : "Invite a reporter you know of out for drinks",
+				"sceneDestination" : "screen_14_wrong"
+			},
+			{
+				"description" : "E-mail the paper asking them to write an article about you",
+				"sceneDestination" : "screen_14_wrong"
+			},
+			{
+				"description" : "Prepare an editorial with your policy ideas and submit it for publication",
+				"sceneDestination" : "screen_14_right"
+			},
+			{
+				"description" : "Show up unannounced at the front desk and demand a meeting with the editor",
+				"sceneDestination" : "screen_14_wrong"
+			},
+			
+	]
+	
+	},
+	{ 
+		"name": "screen_14_wrong",
+		"question" : `Heather responds with “That isn't what I'd do. But I guess you could try it and see what happens."`,
+		"current_date" : "October 2020", // Date for Month
+		"campaign_month_count" : 6, // Counter for Budget
 		"choices" : [
 				{
 					"description" : "Continue",
-					"sceneDestination" : "screen_8"
+					"sceneDestination" : "screen_15"
 				}
 		]
 	
 	},
-
+	{ 
+		"name": "screen_14_right",
+		"question" : `Heather responds with, “Yes! This is exactly what I would suggest!”`,
+		"current_date" : "October 2020", // Date for Month
+		"campaign_month_count" : 6, // Counter for Budget
+		"choices" : [
+				{
+					"description" : "Continue",
+					"sceneDestination" : "screen_15"
+				}
+		]
+	
+	},
+	{ 
+		"name": "screen_15",
+		"question" : `It’s the morning after the 2020 presidential election and the world is abuzz. Although all of the attention is at the White House, you’re organizing for the most local elected level of office. You’re excited to run and want to make it official when you realize
+		How do you qualify for the ballot? `,
+		"current_date" : "November 2020", // Date for Month
+		"campaign_month_count" : 7, // Counter for Budget
+		"choices" : [
+			{
+				"description" : "You pay $10 or collect 25 signatures",
+				"sceneDestination" : "screen_15_right"
+			},
+			{
+				"description" : "You pay $30",
+				"sceneDestination" : "screen_15_wrong"
+			},
+			{
+				"description" : "You pay $10 fee and collect 25 signatures",
+				"sceneDestination" : "screen_15_wrong"
+			},
+			{
+				"description" : "You pay $30 or collect 20 signatures",
+				"sceneDestination" : "screen_15_wrong"
+			},
+			
+	]
+	
+	},
+	{ 
+		"name": "screen_15_wrong",
+		"question" : `Heather responds with “That isn't what I'd do. But I guess you could try it and see what happens."`,
+		"current_date" : "November 2020", // Date for Month
+		"campaign_month_count" : 7, // Counter for Budget
+		"choices" : [
+				{
+					"description" : "Continue",
+					"sceneDestination" : "screen_15"
+				}
+		]
+	
+	},
+	{ 
+		"name": "screen_15_right",
+		"question" : `Heather responds with, “Yes! This is exactly what I would suggest!”`,
+		"current_date" : "November 2020", // Date for Month
+		"campaign_month_count" : 7, // Counter for Budget
+		"choices" : [
+				{
+					"description" : "Continue",
+					"sceneDestination" : "screen_15"
+				}
+		]
+	
+	},
+	{ 
+		"name": "screen_16",
+		"question" : `BOOM. You were hit by a negative TV ad, and your jaw drops at how vicious and untrue it is. No names are listed as the sponsors of the ad, just a PAC. How can you find out who is funding this smear campaign?`,
+		"current_date" : "November 2020", // Date for Month
+		"campaign_month_count" : 7, // Counter for Budget
+		"choices" : [
+			{
+				"description" : "Contact the TV station that aired the ad",
+				"sceneDestination" : "screen_16_right"
+			},
+			{
+				"description" : "Call the current school board",
+				"sceneDestination" : "screen_16_wrong"
+			},
+			{
+				"description" : "Send an e-mail to the county elections clerk",
+				"sceneDestination" : "screen_16_wrong"
+			},
+			{
+				"description" : "Check the Oregon Secretary of State ORESTAR page",
+				"sceneDestination" : "screen_16_wrong"
+			},
+			
+	]
+	
+	},
+	{ 
+		"name": "screen_16_wrong",
+		"question" : `Heather responds with “That isn't what I'd do. But I guess you could try it and see what happens. I'd really suggest checking out ORESTAR at https://sos.oregon.gov/elections/Pages/orestar.aspx"`,
+		"current_date" : "November 2020", // Date for Month
+		"campaign_month_count" : 7, // Counter for Budget
+		"choices" : [
+				{
+					"description" : "Continue",
+					"sceneDestination" : "screen_16"
+				}
+		]
+	
+	},
+	{ 
+		"name": "screen_16_right",
+		"question" : `Heather responds with, “Yes! This is exactly what I would suggest!”`,
+		"current_date" : "November 2020", // Date for Month
+		"campaign_month_count" : 7, // Counter for Budget
+		"choices" : [
+				{
+					"description" : "Continue",
+					"sceneDestination" : "screen_16"
+				}
+		]
+	
+	},
 ];
 export default Scenes;
