4ff58bfcf0839dc08bbf5db8d0f5184df9ca8024
Moarram <papwansam@gmail.com>
2019-04-27 22:17:24 -0700

    added images and fonts

--- src/js/budgetMenu.js
+++ src/js/budgetMenu.js
@@ -1,25 +1,42 @@
 import Sprite from './sprite.js';
+import MoneyData from './money.js';
 export default class BudgetMenu extends Sprite {
 	constructor(game) {
 		super(game);
 		this.expanded = false;
-		this.expandButton = new Button(400, 600, 160, 50, 24, "View Budget");
 		this.budgetItems = [];
-		this.budgetItems.push(new BudgetItem(100, 100, 600, 50, 24, "$200 – Something you can purchase"));
-		this.budgetItems.push(new BudgetItem(100, 170, 600, 50, 24, "$200 – Something else you can purchase"));
+		let offsetY = 100;
+		for (let i in MoneyData) { // TODO
+			this.budgetItems.push(new BudgetItem(100, offsetY, 600, 50, 24, "$200 – Something you can purchase"));
+		}
+		let img = new Image();
+		img.src = './dist/images/BudgetButton.png';
+		img.height = 23;
+		this.showButton = new Button(400, 600, 147, 50, img);
+		img = new Image();
+		img.height = 23;
+		img.src = './dist/images/BudgetUpdateButton.png';
+		this.hideButton = new Button(400, 600, 260, 56, img);
+	}
+	getSelectedIDs() {
+		let ret = [];
+		for (let i in this.budgetItems) {
+			if (this.budgetItems[i].checked) ret.push(i);
+		}
+		return ret;
 	}
 	render() {
 		if (this.expanded) {
-			this.game.ctx.fillStyle = "rgb(0, 0, 0)";
+			this.game.ctx.fillStyle = "#141922";
 			this.game.ctx.fillRect(0, 0, 1440, 800);
 			this.renderTitle(this.game.ctx);
 			this.renderTotals(this.game.ctx);
 			for (let i in this.budgetItems) {
 				this.budgetItems[i].render(this.game.ctx);
 			}
-			this.expandButton.render(this.game.ctx);
+			this.hideButton.render(this.game.ctx);
 		} else {
-			this.expandButton.render(this.game.ctx);
+			this.showButton.render(this.game.ctx);
 		}
 	}
 	click(x, y) {
@@ -27,19 +44,20 @@ export default class BudgetMenu extends Sprite {
 			for (let i in this.budgetItems) {
 				this.budgetItems[i].click(x, y);
 			}
-			if (this.expandButton.isClick(x, y)) {
-				this.expandButton.text = "View Budget";
+			if (this.hideButton.isClick(x, y)) {
+				this.hideButton.visible = false;
+				this.showButton.visible = true;
 				this.expanded = false;
 			}
 			return true;
-		}
-		if (this.expandButton.isClick(x, y)) { // clicked button
-			this.expandButton.text = "Hide Budget";
-			console.log("\'"+this.expandButton.text+"\' button clicked");
+		} else if (this.showButton.isClick(x, y)) { // clicked button
+			this.showButton.visible = false;
+			this.hideButton.visible = true;
 			this.expanded = true;
 			return true;
+		} else {
+			return false;
 		}
-		return false;
 	}
 	// private
 	renderTitle(ctx) {
@@ -58,27 +76,24 @@ export default class BudgetMenu extends Sprite {
 	}
 }
 class Button {
-	constructor(x, y, w, h, pt, text) {
+	constructor(x, y, w, h, img, visible=true) {
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
-		this.pad = 5;
-		this.pt = pt;
-		this.text = text;
-		this.font = pt+"px Arial";
-		this.textColor = "rgb(250, 250, 250)";
-		this.backColor = "rgb(50, 50, 50)";
+		this.img = img;
+		this.pad = 5; // buffer that still counts as click
+		this.backColor = "rgb(50, 50, 50)"; // debug
+		this.visible = visible;
 	}
 	isClick(x, y) {
-		return (x > this.x-this.pad && x < this.x+this.w+this.pad && y > this.y-this.pad && y < this.y+this.h+this.pad);
+		return (this.visible && x > this.x-this.pad && x < this.x+this.w+this.pad && y > this.y-this.pad && y < this.y+this.h+this.pad);
 	}
 	render(ctx) {
+		if (!this.visible) return;
 		ctx.fillStyle = this.backColor;
-		ctx.fillRect(this.x, this.y, this.w, this.h); // background
-		ctx.fillStyle = this.textColor;
-		ctx.font = this.font;
-		ctx.fillText(this.text, this.x+10, (this.y+this.h)-(this.h-this.pt)/2); // center text vertically
+		ctx.fillRect(this.x-this.pad, this.y-this.pad, this.w+(this.pad*2), this.h+(this.pad*2)); // background
+		ctx.drawImage(this.img, this.x, this.y);
 	}
 }
 class BudgetItem {
@@ -89,14 +104,17 @@ class BudgetItem {
 		this.h = h;
 		this.pt = pt;
 		this.text = text;
+
+		this.imgChecked = new Image();
+		this.imgChecked.src = './dist/images/CheckButton_Selected.png';
+		this.imgUnchecked = new Image();
+		this.imgUnchecked.src = './dist/images/CheckButton_Static.png';
+
 		this.textColor = "rgb(250, 250, 250)";
 		this.font = pt+"px Arial";
-		this.csize = 30;
 		this.pad = 5;
 		this.checked = false;
-		this.trueColor = "rgb(250, 250, 250)";
-		this.falseColor = "rgb(100, 100, 100)";
-		this.backColor = "rgb(50, 50, 50)";
+		this.backColor = "rgb(50, 50, 50)"; // debug
 	}
 	click(x, y) {
 		if (x > this.x-this.pad && x < this.x+this.w+this.pad && y > this.y-this.pad && y < this.y+this.h+this.pad) {
@@ -105,11 +123,11 @@ class BudgetItem {
 	}
 	render(ctx) {
 		ctx.fillStyle = this.backColor;
-		ctx.fillRect(this.x-this.pad, this.y-this.pad, this.w+this.pad, this.h+this.pad); // background
-		ctx.fillStyle = (this.checked) ? this.trueColor : this.falseColor;
-		ctx.fillRect(this.x, this.y+(this.h-this.csize)/2, this.csize, this.csize); // checkbox
+		ctx.fillRect(this.x-this.pad, this.y-this.pad, this.w+(this.pad*2), this.h+(this.pad*2)); // background
+		let img = (this.checked) ? this.imgChecked : this.imgUnchecked;
+		ctx.drawImage(img, this.x, this.y+(this.h-img.height)/2); // checkbox
 		ctx.fillStyle = this.textColor;
 		ctx.font = this.font;
-		ctx.fillText(this.text, this.x+this.csize+20, (this.y+this.h)-(this.h-this.pt)/2); // center text vertically
+		ctx.fillText(this.text, this.x+img.width+20, (this.y+this.h)-(this.h-this.pt)/2); // center text vertically
 	}
 }
